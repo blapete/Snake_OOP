4976d96cb28080c3d9e580ad2e50030189e2e0c1
Peebs <blank-p@outlook.com>
2022-11-01 00:36:08 -0500

    main cleaned

--- main.py
+++ main.py
@@ -30,97 +30,6 @@ oApple = Apple()
 oSnake = Snake()
  
 
-# 6
-# while True:
-
-#     # 7 - Check for and handle events
-#     for event in pygame.event.get():
-#         # Clicked the close button? Quit pygame and end program 
-#         if event.type == pygame.QUIT:           
-#             pygame.quit()  
-#             sys.exit()
-
-#     # 8 - Do any "per frame" actions
-    
-#     # 9 - Clear the window
-#     window.fill(BLACK)
-    
-#     # 10 - Draw all window elements
-
-#     # 11 - Update the window
-#     pygame.display.update()
-
-#     # 12 - Slow things down a bit
-#     clock.tick(FRAMES_PER_SECOND)  # make pygame wait
-
-
-
-# class Game():
-
-#     def __init__(self):
-#         self.snake = Snake()
-#         pass
-
-#     def draw(self):
-
-#         screen.fill(BG_COLOR)
-#         oGameView.drawGrid(screen)
-#         self.snake.drawWorm(screen)
-
-#         x = oApple.x * CELLSIZE
-#         y = oApple.y * CELLSIZE
-#         oApple.draw(screen, x, y)
-
-#         z = len(self.snake.wormCoords) - 3
-#         oScore.drawScore(screen, BASICFONT, z)
-        
-#         pygame.display.update()
-#         clock.tick(FRAMES_PER_SECOND)
-
-#     def resetGame(self):
-#         del self.snake
-#         self.snake = Snake()
-#         oApple = Apple()
-#         return True
-
-#     def isGameOver(self):
-#         if (self.snake.wormCoords[self.snake.HEAD]['x'] == -1 or self.snake.wormCoords[self.snake.HEAD]['x'] == CELLWIDTH or self.snake.wormCoords[self.snake.HEAD]['y'] == -1 or self.snake.wormCoords[self.snake.HEAD]['y'] == CELLHEIGHT):
-#             return self.resetGame()
-
-#         for wormBody in self.snake.wormCoords[1:]:
-#             if wormBody['x'] == self.snake.wormCoords[self.snake.HEAD]['x'] and wormBody['y'] == self.snake.wormCoords[self.snake.HEAD]['y']:
-#                 return self.resetGame()
-
-#     def run(self):
-
-
-#         while True:
-#             oStartView.showStartScreen(screen, oController, clock, BASICFONT)
-           
-#             while True:
-#                 for event in pygame.event.get():
-#                     if event.type == pygame.QUIT:
-#                         pygame.quit()
-#                         sys.exit()
-#                     elif event.type == pygame.KEYDOWN:
-#                         oController.handleKeyEvents(event, self.snake)
-
-#                 self.snake.update(oApple)
-
-#                 self.draw()
-                
-#                 if self.isGameOver():
-#                     break
-
-            
-#             oEndView.showGameOver(screen, oController, event, BASICFONT)
-
-
-# game = Game()
-# game.run()
-# pygame.quit()
-# sys.exit()
-
 
 
 oController = Controller(window, clock, BASICFONT)
@@ -139,9 +48,3 @@ while True:
 
 
     oController.draw()
-
-
-    # pygame.display.update()
-
-
-    # clock.tick(FRAMES_PER_SECOND)
